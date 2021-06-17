6609540375ef6d02cd54d57491fe614686149088
iderraz <iderraz@hotmail.com>
2021-06-18 00:26:22 +0200

    formulaire swap entre inscription et connexion

--- public/js/main.js
+++ public/js/main.js
@@ -54,13 +54,49 @@ window.addEventListener("scroll", () =>{
 
 
 
-//inscription
+//connexion
 open.addEventListener("click", () =>{
     modal_container.style.opacity = "1";
+    maNav.style.backgroundColor = "rgb(0, 0, 0, 0.3)";
+    modal_container.style.zIndex = "2";
 });
 
 close.addEventListener("click", () =>{
     modal_container.style.opacity = "0";
+    maNav.style.backgroundColor = "white";
+    modal_container.style.zIndex = "0";
 });
+//connexion
+
+
+//inscription
+let monSecondPop = document.getElementById("modal_container2");
+let inscription = document.getElementById("inscription");
+let connexion3 = document.getElementById("connexion3");
+let closer = document.getElementById("closer");
+
+
+inscription.addEventListener("click", () => {
+    modal_container.style.opacity = "0";
+    modal_container.style.zIndex = "0";
+    monSecondPop.style.opacity = "1";
+    monSecondPop.style.zIndex = "4";
+});
+
+
+connexion3.addEventListener("click", () => {
+    modal_container.style.opacity = "1";
+    modal_container.style.zIndex = "4";
+    monSecondPop.style.opacity = "0";
+    monSecondPop.style.zIndex = "0";
+    
+});
+
+
+closer.addEventListener("click", () =>{
+    monSecondPop.style.opacity = "0";
+    monSecondPop.style.zIndex = "0";
+    maNav.style.backgroundColor = "white";
+})
 //inscription
 
